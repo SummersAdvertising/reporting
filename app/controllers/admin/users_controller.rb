433f498f00aea5248fa72c0dc6c49468e4dc8a51
HanaHsu <kobanae@gmail.com>
2013-06-05 18:42:08 +0800

    admin user manage

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,24 @@ class Admin::UsersController < ApplicationController
 	def index
 		@users = User.all
 	end
+	def new
+		@user = User.new
+	end
+	def create
+		@user = User.new(params[:user])
+
+		respond_to do |format|
+			if(@user.save)
+				format.html { redirect_to admin_users_path }
+				format.json { render json: @user, status: :unprocessable_entity }
+			else
+				format.html { render "new" }
+				format.json { render json: @user.errors, status: :unprocessable_entity }
+			end
+		end
+	end
+	def destroy
+		exit
+		
+	end
 end
